b78ccf4aa61f5eeb061577d6337395de1feb9f71
alone <528636582>
2018-04-11 18:27:38 +0800

    修改bug

--- common/js/public.js
+++ common/js/public.js
@@ -1,52 +1,109 @@
 //头部
 Vue.component('sitenav', {
-    template: '<div class="container siteNav">\
-                 <div class="row" >\
-                 <div class="col-xs-2 col-lg-1 place">\
-                 <div class="dropdown">\
-                 <button type="button" class="btn dropdown-toggle " style="background: none" id="dropdownMenu1"\
-                 data-toggle="dropdown">\
-                 <img src="../../common/img/icon/place.png"/>\
-                 <span>四川·成都</span>\
-                 <span class="caret"></span>\
-                 </button>\
-                 <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">\
-                 <li role="presentation">\
-                 <a role="menuitem" tabindex="-1" href="#">四川·南充</a>\
-                 </li>\
-                 <li role="presentation">\
-                 <a role="menuitem" tabindex="-1" href="#">四川·泸州</a>\
-                 </li>\
-                 </ul>\
-                 </div>\
-                 </div>\
-                 <div class="col-xs-7 col-lg-7 siteNav_welcome">欢迎来到装修加速平台，请 <a href="#" @click="modal_tip()">登录</a> | <a href="#">注册</a></div>\
-                 <div class="col-xs-3 col-lg-4 siteNav_welcome siteNav_right">\
-                 <span class="weixin" @mouseover="wei_img(1)" @mouseout="wei_img(2)">微信公众号</span>\
-                 <div class="weixin_img" v-show="weixin"><img src="../../common/img/icon/weixin.jpg" width="120" height="120"></div>\
-                 </div>\
-                 </div>\
-                 <div class="modal_tip" v-show="isShow">\
-                 <div class="modal_box">\
-                 <div class="modal_close">\
-                 <img @click="modal_close" src="../../common/img/icon/close.png">\
-                 </div>\
-                 <div class="modal_title">账号登录</div>\
-                 <input type="text" class="form-control user" maxlength="11" placeholder="用户名/手机号/邮箱">\
-                 <input type="text" class="form-control passWord" placeholder="密码" >\
-                 <div class="state">\
-                 <input type="checkbox" class="checkbox"><span>下次自动登录</span>\
-                 <div>忘记密码？</div>\
-                 </div>\
-                 <div class="login">立即登录</div>\
-                 <div class="register">还没有账号？<span>免费注册</span></div>\
-                 </div>\
-                 </div>\
-          </div>',
+    template:'<div>\
+    <link rel="stylesheet" href="../../common/css/base.css"/>\
+    <div class="container siteNav">\
+        <div class="row" >\
+            <div class="col-xs-2 col-lg-1 place">\
+                <div class="dropdown">\
+                    <button type="button" class="btn dropdown-toggle " style="background: none" id="dropdownMenu1"\
+                            data-toggle="dropdown">\
+                        <img src="../../common/img/icon/place.png"/>\
+                        <span>四川·成都</span>\
+                        <span class="caret"></span>\
+                    </button>\
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">\
+                        <li role="presentation">\
+                            <a role="menuitem" tabindex="-1" href="#">四川·南充</a>\
+                        </li>\
+                        <li role="presentation">\
+                            <a role="menuitem" tabindex="-1" href="#">四川·泸州</a>\
+                        </li>\
+                    </ul>\
+                </div>\
+            </div>\
+            <div class="col-xs-7 col-lg-7 siteNav_welcome">欢迎来到装修加速平台，请 <a href="#" @click="modal_tip()">登录</a> | <a href="#">注册</a></div>\
+            <div class="col-xs-3 col-lg-4 siteNav_welcome siteNav_right">\
+              <span class="weixin" @mouseover="wei_img(1)" @mouseout="wei_img(2)">微信公众号</span>\
+              <div class="weixin_img" v-show="weixin"><img src="../../common/img/icon/weixin.jpg" width="120" height="120"></div>\
+            </div>\
+        </div>\
+    </div>\
+    <div class="modal_tip"  v-show="isShow" style="display: none">\
+        <div class="modal_box">\
+            <div class="modal_close">\
+                <img @click="modal_close" src="../../common/img/icon/close.png">\
+            </div>\
+            <div class="modal_title">账号登录</div>\
+            <input type="text" class="form-control user" maxlength="11" placeholder="用户名/手机号/邮箱">\
+            <input type="text" class="form-control passWord" placeholder="密码" >\
+            <div class="state">\
+                <input type="checkbox" class="checkbox"><span>下次自动登录</span>\
+                <div>忘记密码？</div>\
+            </div>\
+            <div class="login">立即登录</div>\
+            <div class="register">还没有账号？<span>免费注册</span></div>\
+        </div>\
+    </div>\
+    <div class="l_header">\
+        <nav class="navbar navbar-default" role="navigation">\
+            <div class="container">\
+                <div class="navbar-header">\
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">\
+                        <span class="sr-only">切换导航</span>\
+                        <span class="icon-bar"></span>\
+                        <span class="icon-bar"></span>\
+                        <span class="icon-bar"></span>\
+                    </button>\
+                    <a href="#" class="logo">\
+                        <img src="../../common/img/icon/logo.png">\
+                    </a>\
+                    <div class="person">\
+                        <img src="../../common/img/icon/ok.png" @click="person()">\
+                    </div>\
+                </div>\
+                <div class="collapse navbar-collapse" id="navbar-menu">\
+                    <ul class="nav navbar-nav navbar-left">\
+                        <li><a href="#">首页</a></li>\
+                        <li><a href="#">产品介绍</a></li>\
+                        <li><a href="#">品牌中心</a></li>\
+                        <li><a href="#">设计专区</a></li>\
+                        <li><a href="#">招投标</a></li>\
+                        <li ><a href="#">项目管理</a></li>\
+                        <li ><a href="#">服务商</a></li>\
+                        <li><a href="#">关于我们</a></li>\
+                    </ul>\
+                </div>\
+                <div class="navbar-collapse" id="person">\
+                    <ul class="nav navbar-nav navbar-right">\
+                        <li @click="modal_tip()"><img src="../../common/img/icon/tjn.png" width="50" height="50">\
+                            <br/>\
+                            <br/>\
+                            <div>登录/注册</div>\
+                        </li>\
+                        <li><a href="#">我的关注</a></li>\
+                        <li><a href="#">我的收藏</a></li>\
+                        <li><a href="#">我的消息</a></li>\
+                        <li><a href="#">我的创作</a></li>\
+                        <li ><a href="#">资料与信息</a></li>\
+                    </ul>\
+                </div>\
+            </div>\
+        </nav>\
+        <div class="swiper-container header-swiper-container">\
+                <div class="swiper-wrapper">\
+                    <div class="swiper-slide"><img src="../../common/img/icon/banner1.jpg" width="100%" height="100%"></div>\
+                    <div class="swiper-slide">Slide 2</div>\
+                    <div class="swiper-slide" >Slide 3</div>\
+                </div>\
+                <div class="swiper-pagination header-pagination"></div>\
+            </div>\
+    </div></div>',
     data: function () {
         return {
             isShow:false,
-            weixin:false
+            weixin:false,
+            person_cunt:0
         }
     },
     methods: {
@@ -62,62 +119,29 @@ Vue.component('sitenav', {
             }else {
                 this.weixin=false;
             }
-        }
+        },
+        person:function(){
+            if(this.person_cunt==0){
+                $('#person').slideUp(200);
+                this.person_cunt=1
+            }else {
+                $('#person').slideDown(200);
+                this.person_cunt=0
+            }
+        },
     },
     mounted:function(){
         $('.siteNav').css('display', 'block');
+        var mySwiper = new Swiper('.header-swiper-container',{
+            pagination : '.header-pagination',
+            loop : true,
+            speed:300,
+            keyboardControl : true,
+            mousewheelControl : true,//鼠标控制
+            freeMode : false//切换效果
+            /*  autoplay : 5000,*/
+        });
     }
-});
-//导航
-Vue.component('l_header', {
-    template: '<div class="l_header">\
-                    <nav class="navbar navbar-default" role="navigation">\
-                        <div class="container">\
-                            <div class="navbar-header">\
-                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">\
-                                    <span class="sr-only">切换导航</span>\
-                                    <span class="icon-bar"></span>\
-                                    <span class="icon-bar"></span>\
-                                    <span class="icon-bar"></span>\
-                                    <span class="icon-bar"></span>\
-                                </button>\
-                                <a href="#" class="navbar-brand nav-title">\
-                                </a>\
-                            </div>\
-                            <div class="collapse navbar-collapse" id="navbar-menu">\
-                                <ul class="nav navbar-nav navbar-right">\
-                                    <li><a href="#">首页</a></li>\
-                                    <li><a href="#">产品介绍</a></li>\
-                                    <li><a href="#">品牌中心</a></li>\
-                                    <li><a href="#">设计专区</a></li>\
-                                    <li><a href="#">招投标</a></li>\
-                                    <li ><a href="#">项目管理</a></li>\
-                                    <li ><a href="#">服务商</a></li>\
-                                    <li><a href="#">关于我们</a></li>\
-                                </ul>\
-                            </div>\
-                        </div>\
-                    </nav>\
-                    <div class="swiper-container header-swiper-container">\
-                            <div class="swiper-wrapper">\
-                                <div class="swiper-slide"><img src="../../common/img/icon/banner1.jpg"></div>\
-                                <div class="swiper-slide"><img src="../../common/img/icon/banner1.jpg"></div>\
-                                <div class="swiper-slide" ><img src="../../common/img/icon/banner1.jpg"></div>\
-                            </div>\
-                            <div class="swiper-pagination header-pagination"></div>\
-                        </div>\
-                </div>',
-            mounted:function(){
-                var mySwiper = new Swiper('.header-swiper-container',{
-                    pagination : '.header-pagination',
-                    loop : true,
-                    speed:300,
-                    keyboardControl : true,
-                    mousewheelControl : true,//鼠标控制
-                    freeMode : false,//切换效果
-                      autoplay : 5000,
-                });
-            }
 });
 //底部
 Vue.component('footer-model', {
