32c49f7a1ad846617b73fbb7d05dfab285a829dc
PC-20180411BKZW\Administrator <63203643@qq.com>
2018-04-17 15:49:52 +0800

    zhaolin 4-17

--- pages/brandManage/js/common.js
+++ pages/brandManage/js/common.js
@@ -1,10 +1,84 @@
 var app = new Vue({
     el: '#app',
     data: {
+<<<<<<< HEAD
+        videodata:[
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            },
+            {
+                src:'../../index/video/240.mp4',
+                title:'什么是直播？',
+            }
+        ],
+        luntandata:[
+            {
+                src:'../../common/img/images/img1.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img2.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img1.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img2.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img1.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img2.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img1.png',
+                title:'什么是论坛？',
+            },
+            {
+                src:'../../common/img/images/img2.png',
+                title:'什么是论坛？',
+            },
+        ],
+    },
+    created: function () {
+       
+=======
         
     },
     created: function () {
         
+>>>>>>> 387288fada0fa519d206eb40c155ea2b30be1df1
     },
     methods: {
         //nav高亮
@@ -18,5 +92,46 @@ var app = new Vue({
             //alert(a);
             //window.location.href=''+a+'.html';
         },
+<<<<<<< HEAD
+        //视频
+        clickfun(ev,a){
+            
+            $(ev.target).addClass('myacbgd').parent().siblings().find('a').removeClass('myacbgd');
+            $('.'+a+'').parent().fadeIn();
+            $('.'+a+'').parent().siblings().hide();
+            console.log($('.'+a+''));
+        },
+        playvideo(ev,index){
+
+            var videoobj = document.getElementsByClassName("videoobj")[index];
+            console.log(videoobj);
+            if(videoobj.paused){
+                videoobj.play();
+                //alert()
+            }else{
+                videoobj.pause();
+            }
+        },
+        //更多
+        zkmore(ev){
+            $(ev.target).parent().siblings('.mcontbox').find('.myno').slideToggle();
+        },
+        zhibomore(ev){
+            if(!$(ev.target).hasClass('clicked')){
+                $(ev.target).addClass("clicked");
+                $('.mozhibo').css({'height':'auto'});
+            }else{
+                $(ev.target).removeClass("clicked");
+                $('.mozhibo').css({'height':'107px'});
+            }
+        },
+        //tab
+        myultab(ev,a){
+            $(ev.target).addClass('maccss').siblings('a').removeClass('maccss');
+            $("."+a+"").fadeIn();
+            $("."+a+"").siblings('.multab').hide();
+        },
+=======
+>>>>>>> 387288fada0fa519d206eb40c155ea2b30be1df1
     },
 })
