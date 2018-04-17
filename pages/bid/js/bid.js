b287099b660b1907d1ca828ad62540899dc5a8a0
alone <528636582>
2018-04-17 15:58:34 +0800

    葛亮jun

--- pages/bid/js/bid.js
+++ pages/bid/js/bid.js
@@ -4,16 +4,22 @@
 new Vue({
     el: '#example',
     data:{
-        tenderShow:1
+        tenderShow:1,
+        login_State:1,
     },
     methods: {
         toggle_tender:function(ct){
             this.tenderShow=ct
+        },
+        loginBox:function(c){
+            localStorage.setItem("login_State",c);
+            this.login_State=c;
         }
     },
     mounted:function(){
-        var mySwiper = new Swiper('.swiper-container',{
-            pagination : '.swiper-pagination',
+        this.login_State=localStorage.getItem("login_State");
+        var item_2 = new Swiper('.item_2container',{
+            pagination : '.item_2pagination',
             loop : true,
             speed:300,
             keyboardControl : true,
@@ -21,5 +27,19 @@ new Vue({
             freeMode : false//切换效果
             /*  autoplay : 5000,*/
         });
+        var item_3 = new Swiper('.item_3-container',{
+            loop : true,
+            speed:300,
+            keyboardControl : true,
+            /*  mousewheelControl : true,//鼠标控制*/
+            freeMode : false,//切换效果
+            autoplay : 5000,
+        });
+        var h5_head = new Swiper('.h5_head', {
+            slidesPerView: 4,
+            loop : false,
+            spaceBetween: 0
+        });
+
     }
 });
