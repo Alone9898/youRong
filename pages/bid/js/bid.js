c14a9dc42fc3676316a3e6e3287f3e3880c7f26a
alone <528636582>
2018-04-18 19:10:57 +0800

    葛亮均4.18

--- pages/bid/js/bid.js
+++ pages/bid/js/bid.js
@@ -4,20 +4,12 @@
 new Vue({
     el: '#example',
     data:{
-        tenderShow:1,
-        login_State:1,
+
     },
     methods: {
-        toggle_tender:function(ct){
-            this.tenderShow=ct
-        },
-        loginBox:function(c){
-            localStorage.setItem("login_State",c);
-            this.login_State=c;
-        }
+
     },
     mounted:function(){
-        this.login_State=localStorage.getItem("login_State");
         var item_2 = new Swiper('.item_2container',{
             pagination : '.item_2pagination',
             loop : true,
@@ -35,11 +27,5 @@ new Vue({
             freeMode : false,//切换效果
             autoplay : 5000,
         });
-        var h5_head = new Swiper('.h5_head', {
-            slidesPerView: 4,
-            loop : false,
-            spaceBetween: 0
-        });
-
     }
 });
